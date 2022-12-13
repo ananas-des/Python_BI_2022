4f007586454d8f3cb9142152d1363ce5c129c50a
Anastasia Zolotar <zolotar.ananas@gmail.com>
2022-12-14 02:24:36 +0300

    added shebangs

--- Homework_8/rm.py
+++ Homework_8/rm.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 #!/usr/bin/env python3
 
 
--- Homework_8/touch.py
+++ Homework_8/touch.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 #!/usr/bin/env python3
 
 
