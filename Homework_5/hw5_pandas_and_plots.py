4fec5d87b24717ac9ebe0629110568ce8931a2f5
Anastasia Zolotar <zolotar.ananas@gmail.com>
2023-08-03 17:10:30 +0300

    Minor changes in code

--- Homework_5/hw5_pandas_and_plots.py
+++ Homework_5/hw5_pandas_and_plots.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
 import pandas as pd
@@ -12,6 +9,7 @@ from matplotlib.patches import ConnectionPatch
 import matplotlib.colors as mcolors
 
 
+# Task1. Real Data
 def read_gff(gff_file):
     '''Function read_gff() reads either gziped or not .gff file and transforms it into pandas.DataFrame
     
@@ -21,9 +19,9 @@ def read_gff(gff_file):
     Returns: pandas.DataFrame 
     '''
     
-    df_gff = pd.read_table(gff_file, sep='\t', skiprows=1, names=['chromosome', 'source', 'type', 'start', 
+    df_gff = pd.read_table(gff_file, sep='\t', comment='#', names=['chromosome', 'source', 'type', 'start', 
                                                                   'end', 'score', 'strand', 'phase', 'rRNA_type'])
-    df_gff["rRNA_type"] = df_gff["rRNA_type"].str.extract('(\d{1,2}S)') # extracting type of rRNA from string
+    df_gff["rRNA_type"] = df_gff["rRNA_type"].str.extract('([\d\.]{1,3}S)') # extracting type of rRNA from string
     return df_gff
 
 
@@ -41,18 +39,17 @@ def read_bed6(bed_file):
     return df_bed
 
 
-df_annot = read_gff("rrna_annotation.gff")
+df_annot = read_gff("data/rrna_annotation.gff")
 
-df_aln = read_bed6("alignment.bed")
+df_aln = read_bed6("data/alignment.bed")
 
 # DataFrame with the number of sequences in rRNA types for each reference 
 rRNA_type_chr =  df_annot.groupby('chromosome')['rRNA_type'].value_counts().unstack().fillna(0)
-rRNA_type_chr
 
 # bar plot for rRNA_type_chr
 plt.rcParams["figure.dpi"] = 300
 rRNA_type_chr.plot(kind='bar', xlabel='Sequence', ylabel='Count', 
-                   fontsize=9, figsize=(12, 6))
+                   fontsize=9, figsize=(12, 6));
 
 # DataFrame with contigs ('name' column) that covers reference rRNA
 intersection_df = pd.merge(df_annot, df_aln, on='chromosome', how='inner') # merging two DataFrames on 'chromosome'
@@ -62,8 +59,10 @@ intersection_df = intersection_df[(intersection_df.start_y < intersection_df.sta
                                   & (intersection_df.end_x <= intersection_df.end_y)]
 
 # reading .tsv.gz file with differetial expression data
-diffexp_data = pd.read_table("diffexpr_data.tsv.gz", sep='\t')
+diffexp_data = pd.read_table("data/diffexpr_data.tsv.gz", sep='\t')
 
+
+# Task2. Plot customization
 def exp_changes(row):
     '''Function exp_changes() compares log_pval and logFC from diffexpr_data.tsv.gz 
     with threads and sets expression class for each gene
@@ -93,10 +92,13 @@ def exp_changes(row):
 # adding new column with gene expression class    
 diffexp_data['changes'] = diffexp_data.apply (lambda row: exp_changes(row), axis=1)
 
+# adding new column with gene expression class    
+diffexp_data['changes'] = diffexp_data.apply (lambda row: exp_changes(row), axis=1)
+
 # DataFrame with two the most significantly down- and upregulated genes
 sig_exp = diffexp_data[diffexp_data.changes.str.contains(
     'Significantly', regex=True, na=False)].sort_values(
-    by='logFC', ascending=False).iloc[[0,1,-2,-1]]
+    by='logFC', ascending=False).iloc[[0, 1, -2, -1]]
 
 # grouping order for hue and legend
 hue_order = [
@@ -114,16 +116,16 @@ volcano_plot = sns.scatterplot(
 )
 
 # vertical line (x=0)
-plt.axvline(0, color='grey', linestyle='--', linewidth=1)
+plt.axvline(0, color='grey', linestyle='--', linewidth=2)
 # horizontal line (y=1.301)
-plt.axhline(1.301, color='grey', linestyle='--', linewidth=1)
+plt.axhline(1.301, color='grey', linestyle='--', linewidth=2)
 # limits for x axis
-plt.xlim([min(diffexp_data.logFC)-1, max(diffexp_data.logFC)+1.5])
+plt.xlim([min(diffexp_data.logFC) - 1, max(diffexp_data.logFC) + 1.5])
 
 # legend customization without title
 volcano_plot.legend(
     title=None, shadow=True, 
-    fontsize=10, markerscale=1.5, prop={'weight' : 'bold'}
+    fontsize=10, markerscale=1.5, prop={'weight': 'bold'}
 )
 
 # show minor ticks
@@ -154,16 +156,18 @@ for index, row in sig_exp.iterrows():
     plt.annotate(
         row.Sample, xy=(row.logFC, row.log_pval),
         fontweight='bold',
-        xytext=(row.logFC+0.25, row.log_pval+15),
+        xytext=(row.logFC + 0.25, row.log_pval + 15),
         horizontalalignment='right',
         arrowprops=dict(
             facecolor='red', edgecolor='black',
             width=3, headwidth=7
         )
     )
-    
+
+
+# Task3. Pie Chart
 # importing Top 100 Languages data
-data = pd.read_csv("Top_100_Languages.csv", skiprows=1,
+data = pd.read_csv("data/Top_100_Languages.csv", skiprows=1,
                           names=['Language', 'Total_Speakers',
                                  'Native_Speakers', 'Origin'], decimal=',').drop(
     columns=['Total_Speakers','Origin']).set_index('Language')
@@ -180,12 +184,13 @@ df2 = others_df.sum().rename({'Native_Speakers':'Others'}).to_frame(
     name='Native_Speakers') # summed bottom wedges
 df3 = pd.concat([df1, df2]) #combined DataFrame
 
+
 def calc_coords(wedges, delt = 0.2, x_mul=1.3):
     '''Fuction calc_coords() generates coords for drawing arrows and text boxes'''
     
     angs, xs, ys = [], [], []
     for wedge in wedges:
-        ang = (wedge.theta2 - wedge.theta1)/2. + wedge.theta1
+        ang = (wedge.theta2 - wedge.theta1) / 2. + wedge.theta1
         y = np.sin(np.deg2rad(ang))
         x = np.cos(np.deg2rad(ang))
         angs.append(ang)
@@ -193,29 +198,28 @@ def calc_coords(wedges, delt = 0.2, x_mul=1.3):
         ys.append(y)
         
     nys = ys.copy()
-    for i in range(1,len(ys)-1):
-        if np.sign(xs[i]) != np.sign(xs[i-1]):
-            nys[i] = nys[i-1]
-        elif nys[i-1] - nys[i] < delt and xs[i] <= 0:
-            nys[i] = nys[i-1] - delt
-        elif nys[i-1] - nys[i] <xs[i] > 0:
-            nys[i] = nys[i-1] + delt
+    for i in range(1,len(ys) - 1):
+        if np.sign(xs[i]) != np.sign(xs[i - 1]):
+            nys[i] = nys[i - 1]
+        elif nys[i - 1] - nys[i] < delt and xs[i] <= 0:
+            nys[i] = nys[i - 1] - delt
+        elif nys[i - 1] - nys[i] < xs[i] > 0:
+            nys[i] = nys[i - 1] + delt
     nxs = [x_mul * np.sign(i) for i in xs]
     return angs, [*zip(xs,ys)], [*zip(nxs, nys)]
 
 
-
 shares = df3['Native_Speakers'] / df3['Native_Speakers'].sum()
-explode = [i/10 for i in shares]
+explode = [i / 10 for i in shares]
 # Generate subplots
 fig, (ax1, ax2) = plt.subplots(
-    1, 2, figsize=(12, 6),  dpi=300,
+    1, 2, figsize=(12,6), dpi=300,
     gridspec_kw={'width_ratios': [5, 2]}
 )
 
 # subplot 1 is the piechart itself
 wedges, texts = ax1.pie(
-    df3.Native_Speakers, explode=explode, startangle=180* shares[-1], 
+    df3.Native_Speakers, explode=explode, startangle=180 * shares[-1], 
     colors=sns.color_palette("Set2", n_colors=13)
 )
 
@@ -234,7 +238,7 @@ for i, wedge in enumerate(wedges):
     connectionstyle = "angle,angleA=0,angleB={}".format(angs[i])
     kw["arrowprops"].update({"connectionstyle": connectionstyle})
     ax1.annotate(
-        df3.index[i]+f'\n{shares[i]:.2%}',
+        df3.index[i] + f'\n{shares[i]:.2%}',
         xy=coords[i], xytext=txt_coords[i],
         horizontalalignment='center', **kw)
     
@@ -246,7 +250,7 @@ width = .2
 bar_palette = sns.color_palette("Paired", n_colors=100)
 
 for i, (index, row) in enumerate(others_df.iterrows()):
-    height = row['Native_Speakers']/others_df['Native_Speakers'].sum()
+    height = row['Native_Speakers'] / others_df['Native_Speakers'].sum()
     bottom -= height
     bc = ax2.bar(
         0, height, width, bottom=bottom, 
@@ -277,4 +281,3 @@ for i in [0,1]:
     con.set_color([0, 0, 0])
     con.set_linewidth(0.7)
     ax2.add_artist(con)
-
